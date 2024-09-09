a861fabc1525a67fc1842aa3b015e356740b0fe2
Rinko1231 <89371859+Rinko1231@users.noreply.github.com>
2024-09-09 09:09:19 +0800

    Add files via upload

--- src/main/java/com/teampotato/howdareyouload/HowDareYouLoad.java
+++ src/main/java/com/teampotato/howdareyouload/HowDareYouLoad.java
@@ -1,8 +1,11 @@
 package com.teampotato.howdareyouload;
 
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 
 @Mod(HowDareYouLoad.ID)
 public class HowDareYouLoad {
     public static final String ID = "howdareyouload";
+
 }
